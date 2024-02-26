5bd6f0e94f8a73a38f09e3ef03a311d101c063a6
Bohdan <bohdan.kulchytskyy@seznam.cz>
2024-02-26 13:48:10 +0100

    reordering compiled js files

--- cv1/js/index.js
+++ cv1/js/index.js
@@ -1,26 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var INPUT_LINES_COUNT = 3;
-var INPUT_LINES = [
+const INPUT_LINES_COUNT = 3;
+const INPUT_LINES = [
     '2 * (3+5)',
     '15 - 2**7',
     '5-(5)'
 ];
-var ALLOWED_OPERATORS = '+-*/';
-var checkForUnaryOperators = function (expression) {
-    var expressionCharList = expression.split('');
-    expressionCharList.forEach(function (_char, _index) {
+const ALLOWED_OPERATORS = '+-*/';
+const checkForUnaryOperators = (expression) => {
+    const expressionCharList = expression.split('');
+    expressionCharList.forEach((_char, _index) => {
         if (ALLOWED_OPERATORS.includes(_char)) {
             if (_index === 0 || isNaN(Number(expressionCharList[_index - 1])))
                 throw 'ERROR';
         }
     });
 };
-var customEval = function (expression) {
+const customEval = (expression) => {
     return 'ERROR';
 };
-var evaluateExpression = function (expression) {
-    var expressionWithoutWhitespaces = expression.trim();
+const evaluateExpression = (expression) => {
+    const expressionWithoutWhitespaces = expression.trim();
     try {
         checkForUnaryOperators(expressionWithoutWhitespaces);
         //return customEval(expressionWithoutWhitespaces)
@@ -31,4 +31,4 @@ var evaluateExpression = function (expression) {
     }
 };
 // Printing output
-INPUT_LINES.forEach(function (_line) { return console.log(evaluateExpression(_line)); });
+INPUT_LINES.forEach(_line => console.log(evaluateExpression(_line)));
